efd0c8b41aecacfc6a565a8118b2d8964877fd67
Logan Evans <loganpevans@gmail.com>
2011-11-02 20:32:53 -0700

    branch 'default' changed logan/p220/.red.rb.swp changed logan/p220/red.rb

--- logan/p220/red.rb
+++ logan/p220/red.rb
@@ -5,12 +5,36 @@ $UP =    1
 $LEFT =  2
 $DOWN =  3
 
+$SPACING = 10
+
 def right dir
     (dir + 1) % 4
 end
 
 def left dir
     (dir + 3) % 4
+end
+
+def forward point, dir
+    new_x = nil
+    new_y = nil
+
+    case dir
+    when 0
+        new_x = point.x + $SPACING
+        new_y = point.y
+    when 1
+        new_x = point.x
+        new_y = point.y - $SPACING
+    when 2
+        new_x = point.x - $SPACING
+        new_y = point.y
+    when 3
+        new_x = point.x
+        new_y = point.y + $SPACING
+    end
+
+    Qt::Point.new(new_x, new_y)
 end
 
 class QtApp < Qt::Widget
@@ -25,6 +49,8 @@ class QtApp < Qt::Widget
         resize 350, 280
         move 300, 150
 
+        @loc = center
+
         @button = Qt::PushButton.new 'Push me', self
         connect @button, SIGNAL('clicked()'), self, SLOT('pressed()')
 
@@ -35,24 +61,34 @@ class QtApp < Qt::Widget
         Qt::Point.new((self.size.width / 2), (self.size.height / 2))
     end
 
+    def execute choice
+        case choice
+        when :F
+            @loc = forward @loc, @dir
+        when :L
+            @dir = left @dir
+        when :R
+            @dir = right @dir
+        end
+
+        @loc
+    end
+
     def pressed
-        puts center.x
-        puts center.y
+        update
     end
 
     def paintEvent event
-
         pen = Qt::Painter.new self
 
-        drawPatterns pen
+        drawStep pen
         pen.end
     end
 
-    def drawPatterns pen
+    def drawStep pen
         pen.setPen Qt::Color.new 150, 150, 150
 
-        pen.drawLine(Qt::Point.new(0, 0),
-                     Qt::Point.new(350, 280))
+        pen.drawLine(@loc, (execute :F))
     end
 end
 
