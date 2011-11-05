9ca9e6605271d1cc052152cf49a9a04ab15c3926
Logan Evans <d3veate@gmail.com>
2011-11-05 11:49:32 -0700

    branch 'default' changed logan/p220/.red.rb.swp changed logan/p220/red.rb

--- logan/p220/red.rb
+++ logan/p220/red.rb
@@ -7,38 +7,24 @@ $DOWN =  3
 
 $SPACING = 10
 
-def right dir
-    (dir + 1) % 4
-end
+def gen_directions val
+    directions = "Fa"
+    ret = []
 
-def left dir
-    (dir + 3) % 4
-end
-
-def forward point, dir
-    new_x = nil
-    new_y = nil
-
-    case dir
-    when 0
-        new_x = point.x + $SPACING
-        new_y = point.y
-    when 1
-        new_x = point.x
-        new_y = point.y - $SPACING
-    when 2
-        new_x = point.x - $SPACING
-        new_y = point.y
-    when 3
-        new_x = point.x
-        new_y = point.y + $SPACING
+    (1..val).each do
+        directions.gsub! 'a', 'A'
+        directions.gsub! 'b', 'B'
+        directions.gsub! 'A', 'aRbFR'
+        directions.gsub! 'B', 'LFaLb'
     end
 
-    Qt::Point.new(new_x, new_y)
+    directions.each_char {|c| ret.push c}
+    ret
 end
 
 class QtApp < Qt::Widget
     slots "pressed()"
+    attr_accessor :loc, :dir, :segs
 
     def initialize
         super
@@ -50,45 +36,94 @@ class QtApp < Qt::Widget
         move 300, 150
 
         @loc = center
+        @segs = []
 
         @button = Qt::PushButton.new 'Push me', self
         connect @button, SIGNAL('clicked()'), self, SLOT('pressed()')
 
+        @steps = 0
+        @directions = gen_directions(10)
+        puts @directions
+
         show
+    end
+
+    def next_c
+        @directions.shift
     end
 
     def center
         Qt::Point.new((self.size.width / 2), (self.size.height / 2))
     end
 
-    def execute choice
-        case choice
-        when :F
-            @loc = forward @loc, @dir
-        when :L
-            @dir = left @dir
-        when :R
-            @dir = right @dir
-        end
-
-        @loc
+    def pressed
+        parse!
+        update
     end
 
-    def pressed
-        update
+    def parse!
+        val = next_c
+        case val
+        when 'a'
+            parse!
+        when 'b'
+            parse!
+        when 'F'
+            @steps += 1
+            puts @steps.to_s
+            forward!
+        when 'L'
+            left!
+            parse!
+        when 'R'
+            right!
+            parse!
+        else
+            parse!
+        end
     end
 
     def paintEvent event
         pen = Qt::Painter.new self
-
         drawStep pen
         pen.end
     end
 
     def drawStep pen
         pen.setPen Qt::Color.new 150, 150, 150
+        @segs.each {|seg| pen.drawLine seg}
+    end
 
-        pen.drawLine(@loc, (execute :F))
+    def right!
+        @dir = (@dir + 1) % 4
+    end
+
+    def left!
+        @dir = (@dir + 3) % 4
+    end
+
+    def forward!
+        new_x = nil
+        new_y = nil
+
+        case @dir
+        when 0
+            new_x = @loc.x + $SPACING
+            new_y = @loc.y
+        when 1
+            new_x = @loc.x
+            new_y = @loc.y - $SPACING
+        when 2
+            new_x = @loc.x - $SPACING
+            new_y = @loc.y
+        when 3
+            new_x = @loc.x
+            new_y = @loc.y + $SPACING
+        end
+
+        new = Qt::Line.new(Qt::Point.new(@loc), Qt::Point.new(new_x, new_y))
+        segs.push new
+        @loc = Qt::Point.new new_x, new_y
     end
 end
 
