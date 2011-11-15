0a3e2c26431bf66719df7add6cd282f5f67524bf
Logan Evans <loganpevans@gmail.com>
2011-11-15 12:41:54 -0800

    branch 'default' added logan/p096/doc/created.rid added logan/p096/doc/rdoc-style.css added logan/p263/analyze.py added logan/p263/pecan.py changed logan/p096/glimmering.rb removed p263/analyze.py removed p263/pecan.py

--- logan/p096/glimmering.rb
+++ logan/p096/glimmering.rb
@@ -1,12 +1,54 @@
-class Spot
-    attr_accessor :x, :y, :maybe
-end
-
 class Puzzle
     attr_accessor :board
     def initialize puzzle_hash
         @board = {}
         puzzle_hash.each_pair {|index, val| self[index] = val}
+    end
+
+    def filter group, loc
+        x, y = loc[0], loc[1]
+        done = true
+
+        self.each_spot(group) do |a_x, a_y|
+
+            # each element in my_spot...
+            self.each_spot(group) do |b_x, b_y|
+                next if a_x == b_x and a_y == b_y
+                end
+            end
+        end
+    end
+
+    def get_group group
+        if (1..9).include? group  # Square
+            x, y = (1..3), (1..3) if group == 1
+            x, y = (4..6), (1..3) if group == 2
+            x, y = (7..9), (1..3) if group == 3
+
+            x, y = (1..3), (4..6) if group == 4
+            x, y = (4..6), (4..6) if group == 5
+            x, y = (7..9), (4..6) if group == 6
+
+            x, y = (1..3), (7..9) if group == 7
+            x, y = (4..6), (7..9) if group == 8
+            x, y = (7..9), (7..9) if group == 9
+        elsif (10..18).include? group  # Row
+            x, y = [group - 9], (1..9)
+        elsif (19..27).include? group  # Col
+            x, y = (1..9), [group - 18]
+        else
+            puts "improper group value sent to get_group: #{group}"
+        end
+        return x, y
+    end
+
+    def each_spot group
+        x, y = get_group group
+        x.each do |my_x|
+            y.each do |my_y|
+                yield(my_x, my_y)
+            end
+        end
     end
 
     def [] index
@@ -18,12 +60,13 @@ class Puzzle
     end
 
     def to_s
+        puts '> to_s'
         (1..9).each do |i|
             (1..9).each do |j|
-                print "(#{i}, #{j}): " + self[[i, j]].to_s
+                print "(#{i}, #{j}): " + self[[i, j]].to_s + " "
             end
-            puts
         end
+        puts '< to_s'
     end
 end
 
@@ -52,7 +95,6 @@ def parse_file
             end
             col += 1
         end
-        puts 'r: ' + row.to_s
         row += 1
     end
 
@@ -60,11 +102,18 @@ def parse_file
 end
 
 all_puzzles = parse_file
-a = Puzzle.new all_puzzles[1]
+=begin
+x = Puzzle.new all_puzzles[1]
 
 all_puzzles[1].each_pair do |a, b|
     puts "a: (#{a}) b: (#{b})"
 end
 
-puts a
-puts all_puzzles[3][[1,7]]
+puts '--------------'
+
+puts x.to_s
+=end
+a = Puzzle.new all_puzzles[1]
+
+#puts a
+#puts all_puzzles[3][[1,7]]
