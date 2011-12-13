7bcdfedc1ebd3e15bd5a29c9f20d07fcd7994eef
Logan Evans <loganpevans@gmail.com>
2011-12-12 20:09:30 -0800

    changed logan/p081/rubarb.py

--- logan/p081/rubarb.py
+++ logan/p081/rubarb.py
@@ -1,12 +1,15 @@
-matrix = []
-row = 0
+def parse():
+    matrix = []
+    row = 0
+    with open('./matrix.txt', 'r') as fin:
+        for line in fin.readlines():
+            matrix.append([])
+            for cell in line.split(','):
+                matrix[row].append(int(cell.strip()))
+            row += 1
 
-with open('./matrix.txt', 'r') as fin:
-    print "here"
-    for line in fin.readlines():
-        matrix.append([])
-        for cell in line.split(','):
-            matrix[row].append(int(cell.strip()))
-        row += 1
+    return matrix
 
-print matrix
+
+if __name__ == '__main__':
+    parse()
