edabbd2a758b317296bb9f5d490b157142dc2c37
Logan Evans <loganpevans@gmail.com>
2011-12-12 20:13:07 -0800

    added logan/p081/python_instruction.txt changed logan/p081/rubarb.py

--- logan/p081/rubarb.py
+++ logan/p081/rubarb.py
@@ -1,6 +1,9 @@
 def parse():
+    '''This parses the matrix.txt file into a 2D matrix.'''
+
     matrix = []
     row = 0
+
     with open('./matrix.txt', 'r') as fin:
         for line in fin.readlines():
             matrix.append([])
@@ -11,5 +14,9 @@ def parse():
     return matrix
 
 
+def reduce(matrix):
+    
+
+
 if __name__ == '__main__':
-    parse()
+    print parse()
