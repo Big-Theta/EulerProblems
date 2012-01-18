d0efe9bb36c2de8834d71009fc4ee7d2d8bf77fa
cevans87 <cameron_evans87@hotmail.com>
2012-01-18 00:31:09 -0800

    Added solution to p83. I just whipped most of it out in like 10 minutes, and the solution rocks. Pimp status!

--- cameron/p083/p83.rb
+++ cameron/p083/p83.rb
@@ -4,33 +4,29 @@ def problem83
   m, m_dim = gimme_matrix
   path_sum = Hash.new(2**32)
   path_sum[[0, 0]] = m[[0, 0]]
-  (1..m_dim[1]).each do |y|
-    path_sum[[0, y]] = m[[0, y]] + path_sum[[0, y - 1]]
-  end
-  (1..m_dim[0]).each do |x|
-    update_min_to_column(x, path_sum, m, m_dim[0], m_dim[1])
+  current_column = 0
+  while current_column < m_dim[0] + 1
+    current_column = update_column(current_column, path_sum, m, m_dim[0], m_dim[1])
   end
   path_sum[[m_dim[0], m_dim[1]]]
 end
 
-def update_min_to_column(x, path_sum, m, dim_x, dim_y)
-  path_sum[[x, 0]] = path_sum[[x - 1, 0]] + m[[x, 0]]
-  1.upto(dim_y) do |y|
-    path_sum[[x, y]] = m[[x, y]] + [path_sum[[x, y - 1]], path_sum[[x - 1, y]]].min
-  end
-  path_sum[[x, dim_y]] = path_sum[[x - 1, dim_y]] + m[[x, dim_y]]
-  (dim_y - 1).downto(0) do |y|
-    path_sum[[x, y]] = [m[[x, y]] + [path_sum[[x - 1, y]], path_sum[[x, y + 1]]].min, path_sum[[x, y - 1]]].min
-  end
-  update_prev_column(x - 1, path_sum, m, dim_x, dim_y)
-end
-
-def update_prev_column(x, path_sum, m, dim_x, dim_y)
-  if x > 0
-    has_changed = false
-    0.upto(dim_y) do |y|
-      from_right = path_sum[[x + 1, y]] + m[[x, y]]
-      if from_right < 
+def update_column(x, path_sum, m, dim_x, dim_y)
+  modified = false
+  0.upto(dim_y) do |y|
+    possible = m[[x, y]] + min_adjacent(x, y, path_sum)
+    if possible < path_sum[[x, y]]
+      modified = true
+      path_sum[[x, y]] = possible
     end
   end
+  if modified == true and x > 0
+    update_column(x - 1, path_sum, m, dim_x, dim_y)
+  else
+    x + 1
+  end
+end
+
+def min_adjacent(x, y, ps)
+  [ps[[x - 1, y]], ps[[x + 1, y]], ps[[x, y - 1]], ps[[x, y + 1]]].min
 end
