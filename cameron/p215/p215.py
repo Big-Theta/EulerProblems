ea7fecd44e829b2b025ef18ff34971af2c68efc7
Cameron Evans <c.d.evans87@gmail.com>
2012-03-12 01:13:52 -0700

    changed cameron/p215/p215.py

--- cameron/p215/p215.py
+++ cameron/p215/p215.py
@@ -5,22 +5,29 @@ from brick import *
 def p215(width, height):
     wall = []
     build_wall(width, [0], wall)
-    print wall[0]
-    print wall[-1]
-    print "wall length is %i" % len(wall)
+    path_count = [1] * len(wall)
+    print "Done building wall. %i possible rows" % len(wall)
     adj = {}
     for i, row in enumerate(wall):
         adj[i] = []
         for j, possible_row in enumerate(wall):
             if not intersect(row, possible_row):
                 adj[i] = adj[i] + [j]
-                """
-                print "Match"
-                print row
-                print possible_row
-                print ""
-                """
-        print str(i) + " " + str(adj[i])
+        #print str(i) + " " + str(adj[i])
+    print "Done finding adjacencies."
+    for x in range(1, height):
+        path_count = add_routes(path_count, adj)
+    total_paths = 0
+    for x in path_count:
+        total_paths += x
+    print total_paths
+
+def add_routes(current_count, adj):
+    path_count = [0] * len(current_count)
+    for key, adjacencies in adj.iteritems():
+        for adjacency in adjacencies:
+            path_count[key] += current_count[adjacency]
+    return path_count
 
 def build_wall(width, breaks, rows):
     if breaks[-1] < width:
