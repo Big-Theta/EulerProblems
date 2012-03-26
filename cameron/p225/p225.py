a76a3b6e35e548f568c9fb256b1d6b316cb9956c
Cameron Evans <c.d.evans87@gmail.com>
2012-03-26 02:41:29 -0700

    changed cameron/p225/p225.py

--- cameron/p225/p225.py
+++ cameron/p225/p225.py
@@ -30,26 +30,23 @@ def p225_old(answers_needed):
 
 def p225(answers_needed):
     seq = [1, 1, 1]
+    remainders = [1, 1, 1]
     cur_divisor = 17
     answers_found = 0
 
-    remainders = [1, 1, 1]
-    cur_index = 3
     while answers_found < answers_needed:
-        seq = get_seq(cur_index + 1, seq)
-        remainders = remainders[1:] + [seq[cur_index] % cur_divisor]
+        seq = seq[1:] + [seq[0] + seq[1] + seq[2]]
+        remainders = remainders[1:] + [seq[-1] % cur_divisor]
         if remainders[-1] == 0:
-            #print "found a dud %i" % cur_divisor
-            cur_divisor += 2
+            seq = [1, 1, 1]
             remainders = [1, 1, 1]
-        elif remainders == [1, 1, 1] and cur_index > 3:
+            cur_divisor += 2
+        elif remainders == [1, 1, 1]:
+            print "Yay, found %i, divisor number %i" % (cur_divisor, answers_found + 1)
             answers_found += 1
-            print "Yay, found %i, divisor number %i" % (cur_divisor, answers_found)
             cur_divisor += 2
+            seq = [1, 1, 1]
             remainders = [1, 1, 1]
-            cur_index = 2
-        cur_index += 1
-    print cur_divisor
 
 def get_seq(length, seq):
     while len(seq) < length:
