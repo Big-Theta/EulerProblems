a6ac69dc6337ef2f3c3c10b580d796bed01b67db
Cameron Evans <c.d.evans87@gmail.com>
2012-04-07 01:27:24 -0700

    changed cameron/p225/p225.py

--- cameron/p225/p225.py
+++ cameron/p225/p225.py
@@ -2,57 +2,24 @@
 
 from primes import *
 
-def p225_old(answers_needed):
-    seq = [1, 1, 1]
-    cur_divisor = 17
-    answers_found = 0
-
-    remainders = [1, 1, 1]
-    while answers_found < answers_needed:
-        seq = get_seq((len(remainders) + 1) * 2, seq)
-        remainders += [seq[len(remainders)] % cur_divisor]
-        if remainders[-1] == 0:
-            #print "found a dud %i" % cur_divisor
-            cur_divisor += 2
-            remainders = [1, 1, 1]
-        elif remainders[-3:] == [1, 1, 1]:
-            #print "gets here!"
-            target = remainders[:-3]
-            possible_match = [x % cur_divisor for x in seq[len(target):len(target) * 2]]
-            #print target
-            #print possible_match
-            if target == possible_match:
-                answers_found += 1
-                print "Yay, found %i, divisor number %i" % (cur_divisor, answers_found)
-                cur_divisor += 2
-                remainders = [1, 1, 1]
-    print cur_divisor
-
 def p225(answers_needed):
-    seq = [1, 1, 1]
-    remainders = [1, 1, 1]
+    rems = [1, 1, 3]
     cur_divisor = 17
     answers_found = 0
 
     while answers_found < answers_needed:
-        seq = seq[1:] + [seq[0] + seq[1] + seq[2]]
-        remainders = remainders[1:] + [seq[-1] % cur_divisor]
-        if remainders[-1] == 0:
-            seq = [1, 1, 1]
-            remainders = [1, 1, 1]
-            cur_divisor += 2
-        elif remainders == [1, 1, 1]:
-            print "Yay, found %i, divisor number %i" % (cur_divisor, answers_found + 1)
-            answers_found += 1
-            cur_divisor += 2
-            seq = [1, 1, 1]
-            remainders = [1, 1, 1]
+        cur_divisor += 2
+        rems = [1, 1, 3]
+        i = 0
+        while not 0 in rems:
+            rems[i] = (rems[0] + rems[1] + rems[2]) % cur_divisor
+            i = (i + 1) % 3
+            if rems == [1, 1, 1]:
+                rems = [0, 0, 0]
+                answers_found += 1
+                if answers_found == answers_needed:
+                    print cur_divisor
 
-def get_seq(length, seq):
-    while len(seq) < length:
-        seq += [seq[-3] + seq[-2] + seq[-1]]
-        #print seq[-1]
-    return seq
 
 if __name__ == "__main__":
     p225(124)
