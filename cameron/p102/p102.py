b2a70e18dadadaff13d3f1ef6fbaeb2e7849d3ea
Cameron Evans <c.d.evans87@gmail.com>
2012-09-13 21:28:51 -0700

    changed cameron/p102/p102.py

--- cameron/p102/p102.py
+++ cameron/p102/p102.py
@@ -2,7 +2,15 @@
 
 def p102():
     f = open("triangles.txt")
-    print f
+    f = f.readlines()
+    for line in f:
+        tri = line.strip().split(',')
+        for i, x in enumerate(tri):
+            tri[i] = int(x)
+        print tri
+
+def contains_origin(p1, p2, p3):
+    
 
 if __name__ == "__main__":
     p102()
